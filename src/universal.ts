5f6221749e6077c4cc05822ac932c31cc59a9803
Richa-iitr <richa@cs.iitr.ac.in>
2023-12-09 23:52:03 +0530

    graph

--- src/universal.ts
+++ src/universal.ts
@@ -4,52 +4,37 @@ import {
   productReviewed,
   thresholdChanged
 } from "../generated/Universal/Universal"
-import { ExampleEntity } from "../generated/schema"
+import { ProductState, ThresholdChanged } from "../generated/schema"
 
 export function handleproductReviewed(event: productReviewed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let id = event.params.productId.toString()
+  let entity = ProductState.load(id)
+  if (!entity) { 
+    entity = new ProductState(id)
+    entity.countReviews = BigInt.fromI32(0)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  entity.countReviews = entity.countReviews + BigInt.fromI32(1)
 
-  // Entity fields can be set based on event parameters
   entity.productId = event.params.productId
-  entity.sender = event.params.sender
-
-  // Entities can be written to the store with `.save()`
+  let reviewers = entity.reviewers
+  reviewers.push(event.params.sender)
+  entity.reviewers = reviewers
   entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.claimThreshold(...)
-  // - contract.getNoOfReview(...)
-  // - contract.noOfReviews(...)
-  // - contract.products(...)
-  // - contract.token(...)
 }
 
-export function handlethresholdChanged(event: thresholdChanged): void {}
+export function handlethresholdChanged(event: thresholdChanged): void {
+  let entity = ThresholdChanged.load(BigInt.fromI32(0).toString())
+  if(!entity) { 
+    entity = new ThresholdChanged(BigInt.fromI32(0))
+    entity.prevId = BigInt.fromI32(0)
+    entity.threshold = event.params.threshold
+    entity.save()
+  } else {
+    let id = BigInt.fromI32(entity.prevId.plus(BigInt.fromI32(1))).toString()
+    let newEntity = new ThresholdChanged(id)
+    newEntity.prevId = entity.prevId.plus(BigInt.fromI32(1))
+    newEntity.threshold = event.params.threshold
+    newEntity.save()
+  }
+}
